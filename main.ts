0674c83b65e8712ee0ef0af3530a8b5d0fc59979
tennirb <gabriel.castro@pioneiro.g12.br>
2025-05-05 21:13:19 -0300

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,56 +1,86 @@
 let Fernando = sprites.create(assets.image`myImage`, SpriteKind.Player)
+let statusbar = statusbars.create(10, 3, StatusBarKind.Energy)
+statusbar.attachToSprite(Fernando)
 controller.moveSprite(Fernando, 100, 100)
 tiles.setCurrentTilemap(tilemap`level`)
 scene.cameraFollowSprite(Fernando)
-story.printCharacterText("Fernando inicia sua aventura no acampamento de escoteiros", "Narrador")
-forever(function () {
-    characterAnimations.loopFrames(
-    Fernando,
-    [img`
-        . . . . . . . . . . . . . . . . 
-        . . . . f f f f f . . . . . . . 
-        . . . f f f f f f f f . . . . . 
-        . . . f f f f f f f f . . . . . 
-        . . . . f e e e e e f . . . . . 
-        . . . . f 1 f e f 1 f . . . . . 
-        . . . . e 1 f e f 1 e . . . . . 
-        . . . . e e e e e e e . . . . . 
-        . . . . . . 6 e 8 . . . . . . . 
-        . . . . e e 9 9 9 6 e . . . . . 
-        . . . e e 9 9 9 6 e e . . . . . 
-        . . e e . 9 9 9 9 e e e . . . . 
-        . . . . . 9 9 9 9 e e e . . . . 
-        . . . . . d d d d d . . . . . . 
-        . . . . . d d . d d . . . . . . 
-        . . . . . e e . e e . . . . . . 
-        `],
-    500,
-    characterAnimations.rule(Predicate.MovingLeft)
-    )
+story.startCutscene(function () {
+    story.printCharacterText("Fernando tenta construir uma cabana em seu quintal, e quando foi dormir dentro dela, choveu e teve uma gripe forte, agora seu pai quer o colocar no grupo  de escoteiros, junto com seu irmão Toninho.", "Narrador")
 })
 forever(function () {
     characterAnimations.loopFrames(
     Fernando,
     [img`
         . . . . . . . . . . . . . . . . 
-        . . . . . . f f f f f . . . . . 
-        . . . . f f f f f f f f . . . . 
-        . . . . f f f f f f f f . . . . 
-        . . . . f f f f f f f . . . . . 
-        . . . . f f f f f f f . . . . . 
-        . . . . e e f f f e e . . . . . 
-        . . . . e e e e e e e . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . f e e . . . . . . . 
+        . . . . . . f e e . . . . . . . 
+        . . . . . . e e e . . . . . . . 
         . . . . . . 6 e 8 . . . . . . . 
-        . . . . e e 9 9 9 6 e . . . . . 
-        . . . e e 9 9 9 6 e e . . . . . 
-        . . e e . 9 9 9 9 e e e . . . . 
-        . . . . . 9 9 9 9 e e e . . . . 
-        . . . . . d d d d d . . . . . . 
-        . . . . . d d . d d . . . . . . 
-        . . . . . e e . e e . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . . 9 9 6 6 . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . . d d d . . . . . . . 
+        . . . . . . e . e . . . . . . . 
+        . . . . . . e . . e . . . . . . 
+        . . . . . . e . . e . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . f e e . . . . . . . 
+        . . . . . . f e e . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . 6 e 8 . . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . . 9 9 6 6 . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . . d d d . . . . . . . 
+        . . . . . . e . e . . . . . . . 
+        . . . . . . e . . e . . . . . . 
+        . . . . . . e . . . e . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . f e e . . . . . . . 
+        . . . . . . f e e . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . 6 e 8 . . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . . 9 9 6 6 . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . . d d d . . . . . . . 
+        . . . . . . e . e . . . . . . . 
+        . . . . . . e . . e . . . . . . 
+        . . . . . . e . . e e . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . f e e . . . . . . . 
+        . . . . . . f e e . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . 6 e 8 . . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . . 9 9 6 6 . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . . d d d . . . . . . . 
+        . . . . . . e . e . . . . . . . 
+        . . . . . . e . . e . . . . . . 
+        . . . . . . e . . . e . . . . . 
         `],
-    500,
-    characterAnimations.rule(Predicate.MovingUp)
+    300,
+    characterAnimations.rule(Predicate.MovingLeft)
     )
 })
 forever(function () {
@@ -83,6 +113,31 @@ forever(function () {
     Fernando,
     [img`
         . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f f . . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . e e f f f e e . . . . . 
+        . . . . e e e e e e e . . . . . 
+        . . . . . . 6 e 8 . . . . . . . 
+        . . . . e e 9 9 9 6 e . . . . . 
+        . . . e e 9 9 9 6 e e . . . . . 
+        . . e e . 9 9 9 9 e e e . . . . 
+        . . . . . 9 9 9 9 e e e . . . . 
+        . . . . . d d d d d . . . . . . 
+        . . . . . d d . d d . . . . . . 
+        . . . . . e e . e e . . . . . . 
+        `],
+    500,
+    characterAnimations.rule(Predicate.MovingUp)
+    )
+})
+forever(function () {
+    characterAnimations.loopFrames(
+    Fernando,
+    [img`
+        . . . . . . . . . . . . . . . . 
         . . . . . . f f f . . . . . . . 
         . . . . . . f f f . . . . . . . 
         . . . . . . f f f . . . . . . . 
@@ -92,14 +147,99 @@ forever(function () {
         . . . . . . e e e . . . . . . . 
         . . . . . . 6 e 8 . . . . . . . 
         . . . . . . 9 9 9 . . . . . . . 
-        . . . . . . 9 9 6 . . . . . . . 
-        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . 6 6 9 9 . . . . . . . 
         . . . . . . 9 9 9 . . . . . . . 
         . . . . . . d d d . . . . . . . 
-        . . . . . . d e d . . . . . . . 
+        . . . . . . e . e . . . . . . . 
+        . . . . . e . . e . . . . . . . 
+        . . . . . e . . e . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
         . . . . . . e e e . . . . . . . 
+        . . . . . . e e f . . . . . . . 
+        . . . . . . e e f . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . 6 e 8 . . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . 6 6 9 9 . . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . . d d d . . . . . . . 
+        . . . . . . e . e . . . . . . . 
+        . . . . . e . . e . . . . . . . 
+        . . . . e . . e . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . e e f . . . . . . . 
+        . . . . . . e e f . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . 6 e 8 . . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . 6 6 9 9 . . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . . d d d . . . . . . . 
+        . . . . . . e . e . . . . . . . 
+        . . . . . e e . e . . . . . . . 
+        . . . . e e . . e . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . e e f . . . . . . . 
+        . . . . . . e e f . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . 6 e 8 . . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . 6 6 9 9 . . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . . d d d . . . . . . . 
+        . . . . . . e . e . . . . . . . 
+        . . . . . e e . e . . . . . . . 
+        . . . . e e . . e . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . e e f . . . . . . . 
+        . . . . . . e e f . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . 6 e 8 . . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . 6 6 9 9 . . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . . d d d . . . . . . . 
+        . . . . . . e . e . . . . . . . 
+        . . . . . e . . e . . . . . . . 
+        . . . . e . . e . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . e e f . . . . . . . 
+        . . . . . . e e f . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . 6 e 8 . . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . 6 6 9 9 . . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . . d d d . . . . . . . 
+        . . . . . . e . e . . . . . . . 
+        . . . . . e . . e . . . . . . . 
+        . . . . e . . e e . . . . . . . 
         `],
-    500,
+    300,
     characterAnimations.rule(Predicate.MovingRight)
     )
 })
